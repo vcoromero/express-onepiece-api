2cc6f7cc70511fa99c58042af2c711e10a1688e7
Victor Romero <vcoromero@gmail.com>
2025-10-13 00:08:33 -0600

    feat: add sequelize model for fruit types
    - Create FruitType model with schema definition
    - Add field validations (name: 1-50 chars, description: optional)
    - Configure table name and timestamps
    - Add model index file for easy imports

---
+++ src/models/index.js
@@ -0,0 +1,18 @@
+/**
+ * Models Index
+ * Exports all Sequelize models
+ */
+
+const FruitType = require('./fruit-type.model');
+
+// Future models can be added here:
+// const Character = require('./character.model');
+// const DevilFruit = require('./devil-fruit.model');
+// const Organization = require('./organization.model');
+// etc...
+
+module.exports = {
+  FruitType
+  // Add more models as they are created
+};
+
