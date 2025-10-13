846ae0e8354af9b1172f45c41bb15c4b55dc1183
Victor Romero <vcoromero@gmail.com>
2025-10-12 23:04:20 -0600

    test: configure global mocks for console and logger in jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,27 @@ require('dotenv').config({
   path: './configs/.env.test'
 });
 
+// Mock console methods globally to suppress noise in test output
+// These are already tested or not relevant to test
+jest.spyOn(console, 'error').mockImplementation(() => {});
+jest.spyOn(console, 'log').mockImplementation(() => {});
+jest.spyOn(console, 'warn').mockImplementation(() => {});
+jest.spyOn(console, 'info').mockImplementation(() => {});
+
+// Mock logger globally - it has its own tests and we don't want logging during tests
+jest.mock('./src/utils/logger', () => ({
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+  debug: jest.fn(),
+  http: jest.fn(),
+  security: {
+    loginSuccess: jest.fn(),
+    loginFailed: jest.fn(),
+    tokenVerified: jest.fn(),
+    tokenInvalid: jest.fn(),
+    rateLimitExceeded: jest.fn(),
+    unauthorizedAccess: jest.fn()
+  },
+  httpRequest: jest.fn()
+}));
